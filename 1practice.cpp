a2bac32648e3adf43c3d16368fad24d6c73b3659
skozlovtsev <78878805+skozlovtsev@users.noreply.github.com>
2021-02-18 14:21:23 +0300

    Add files via upload
    1 практическая работа по Технологии программирования

---
+++ 1practice.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+/*ex 1
+int main() {
+	int a;
+	float b;
+	std::string c;
+	long int d;
+
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+	std::cin >> d;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+	return 0;
+}
+*/
+
+/* ex 7
+int main() {
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << std::sqrt(std::pow(a, 2) + std::pow(b, 2)) << std::endl;
+	return 0;
+}
+*/
+/* ex 9
+int func(int x) {
+	return std::pow(x, 2) + (3 * x) - 100;
+};
+
+
+int main() {
+	int a;
+	std::cin >> a;
+	std::cout << func(a) << std::endl;
+	return 0;
+}
+*/
+/* ex 11
+int main() {
+	using namespace std;
+	int a, b, c, d;
+	float x1, x2;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	d = pow(b,2) - (4 * a * c);
+	if(d >= 0){
+		x1 = (-b - sqrt(d)) / (2 * a);
+		x2 = (-b + sqrt(d)) / (2 * a);
+		cout << x1 << " " << x2 << endl;
+	}
+	else{
+		cout << "NULL" << endl;
+	}
+    return 0;
+}
+*/
+
